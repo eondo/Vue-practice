cc0bae72da99accc9e61e2ea09e7241ac669fc98
eondo <vov06144@naver.com>
2022-11-09 17:56:25 +0900

    Update

--- vue_js_youtube/vue-js-youtube/src/App.vue
+++ vue_js_youtube/vue-js-youtube/src/App.vue
@@ -1,8 +1,31 @@
 <template>
   <div id="app">
+    <nav class="navbar navbar-light bg-light justify-content-between">
+      <a class="navbar-brand" href="#">
+        <img
+          src="/docs/4.0/assets/brand/bootstrap-solid.svg"
+          width="30"
+          height="30"
+          class="d-inline-block align-top"
+          alt=""
+        />
+        Bootstrap
+      </a>
+      <form class="form-inline">
+        <input
+          class="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+          Search
+        </button>
+      </form>
+    </nav>
     <SearchBar @search="search" />
-    <MainVideo :searched-main="searchedMain"/>
-    <VideoList @go-to-main="goToMain" :searched-list="searchedList"/>
+    <MainVideo :searched-main="searchedMain" />
+    <VideoList @go-to-main="goToMain" :searched-list="searchedList" />
   </div>
 </template>
 
@@ -40,22 +63,24 @@ export default {
           part: "snippet",
           type: "video",
           q: searchSubject,
+          maxResults: 10,
         },
       }).then((response) => {
         console.log(response); // 응답은 총 5개의 {}가 data의 items라는 배열에 들어있음 [{}, {}, {}]
-        this.searchedList = response.data.items.slice(1) // 검색결과리스트에다가 response.data를 저장
-        this.searchedMain = response.data.items[0]
+        this.searchedList = response.data.items.slice(1); // 검색결과리스트에다가 response.data를 저장
+        this.searchedMain = response.data.items[0];
       });
     },
     goToMain(video) {
       const newSearchedList = this.searchedList.map((searched) => {
         if (searched === video) {
-          searched = this.searchedMain
-        } return searched
-      })
-      this.searchedMain = video
-      this.searchedList = newSearchedList
-    }
+          searched = this.searchedMain;
+        }
+        return searched;
+      });
+      this.searchedMain = video;
+      this.searchedList = newSearchedList;
+    },
   },
 };
 </script>
